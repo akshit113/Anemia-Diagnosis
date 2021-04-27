622877db0de96f121ef6f519a55c5ef159164e30
akshit-agarwal-123 <akshit@email.arizona.edu>
2021-04-27 00:38:24 -0600

    chore: get age bins

--- lr.py
+++ lr.py
@@ -1,5 +1,26 @@
-from pandas import read_csv
-from pandas_profiling import ProfileReport
+from pandas import read_csv, cut
+
+
+# from pandas_profiling import ProfileReport
+
+
+def get_bins(df, col, newcol, intervals, labels):
+    """Creates bins of a specific column in dataframe with speciifed intervals and labels
+    Args:
+        df: dataframe
+        col: column name to be binned
+        newcol: new column to created with bins
+        intervals: bin intervals
+        labels: bin labels
+
+    Returns: a Dataframe with bins"""
+
+    cur_idx = list(df.columns).index(col)
+    new_idx = cur_idx + 1
+    df[newcol] = cut(df[col], bins=intervals, labels=labels)
+    rm_df = df.pop(newcol)
+    df.insert(new_idx, newcol, rm_df)
+    return df
 
 
 def import_data(fname):
@@ -15,17 +36,19 @@ def import_data(fname):
     return df
 
 
-def visualize(df):
-    profile = ProfileReport(df, title="Pandas Profiling Report")
-    profile.to_file('profile.html')
-    print('')
+# def visualize(df):
+#     profile = ProfileReport(df, title="Pandas Profiling Report", explorative=True)
+#     profile.to_file('profile.html')
+#     print('')
 
 
 if __name__ == '__main__':
     fpath = 'dataset/data.csv'
     df = import_data(fname=fpath)
-    visualize(df)
+    # visualize(df)
     print(list(df.columns))
+    df = get_bins(df, col='Age', newcol='age_type', intervals=[0, 18, 30, 60, 200],
+                  labels=['young', 'adult', 'mature', 'retired'])
 
     print(df.head())
 
