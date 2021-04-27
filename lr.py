77328500c8918142df66e3cff376ae290125871c
akshit-agarwal-123 <akshit@email.arizona.edu>
2021-04-27 01:20:04 -0600

    chore: normalize and one-hot encode

--- lr.py
+++ lr.py
@@ -1,10 +1,10 @@
-from pandas import read_csv, cut
-
+from pandas import read_csv, cut, get_dummies, concat, DataFrame
 
 # from pandas_profiling import ProfileReport
+from sklearn.preprocessing import MinMaxScaler
 
 
-def get_bins(df, col, newcol, intervals, labels):
+def get_bins(df, col, newcol, intervals, labels, keep=False):
     """Creates bins of a specific column in dataframe with speciifed intervals and labels
     Args:
         df: dataframe
@@ -12,6 +12,7 @@ def get_bins(df, col, newcol, intervals, labels):
         newcol: new column to created with bins
         intervals: bin intervals
         labels: bin labels
+        keep: keep original values. can be true or false
 
     Returns: a Dataframe with bins"""
 
@@ -20,6 +21,8 @@ def get_bins(df, col, newcol, intervals, labels):
     df[newcol] = cut(df[col], bins=intervals, labels=labels)
     rm_df = df.pop(newcol)
     df.insert(new_idx, newcol, rm_df)
+    if not keep:
+        df.drop([col], inplace=True, axis=1)
     return df
 
 
@@ -42,6 +45,41 @@ def import_data(fname):
 #     print('')
 
 
+def normalize_columns(df, colnames):
+    """Performs Normalization using MinMaxScaler class in Sckikit-learn"""
+    scaler = MinMaxScaler()
+    for col in colnames:
+        x = df.loc[:, [col]]
+        x = df[[col]].values.astype(float)
+        x_scaled = scaler.fit_transform(x)
+        df[col] = DataFrame(x_scaled)
+    print(f'''Normalized Columns: {colnames} using MinMaxScaler.''')
+    return df
+
+
+def one_hot_encode(df, colnames):
+    """This function performs one-hot encoding of the columns
+    :param df: input df
+    :param colnames: columns to be one-hot encoded
+    :return: dataframe
+    """
+
+    for col in colnames:
+        oh_df = get_dummies(df[col], prefix=col, drop_first=True)
+        df = concat([oh_df, df], axis=1)
+        df = df.drop([col], axis=1)
+    missing = (df.isnull().values.any())
+    while missing:
+        df = df.dropna()
+        print(df.isnull().sum())
+        missing = (df.isnull().values.any())
+
+    print(df.shape)
+    print(list(df.columns))
+    print(df.shape)
+    return df
+
+
 if __name__ == '__main__':
     fpath = 'dataset/data.csv'
     df = import_data(fname=fpath)
@@ -50,6 +88,8 @@ if __name__ == '__main__':
     df = get_bins(df, col='Age', newcol='age_type', intervals=[0, 18, 30, 60, 200],
                   labels=['young', 'adult', 'mature', 'retired'])
 
+    df = normalize_columns(df, colnames=['PCV', 'MCV', 'MCH', 'MCHC', 'RDW', 'TLC', 'PLT/mm3', 'HGB'])
+    df = one_hot_encode(df, colnames=['age_type'])
     print(df.head())
 
     print('')
